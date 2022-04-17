2ab7d4f84a5b4ef6f6e23c37ff072c5beac8475f
noman081 <abdullahalnoman642@gmail.com>
2022-04-17 16:55:35 +0600

    After completing slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Home from './Pages/Home/Home/Home';
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-
+        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
 
       <Footer></Footer>
---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import slider1 from '../../../images/Slider/slider1.jpg';
+import slider2 from '../../../images/Slider/slider2.jpg';
+import slider3 from '../../../images/Slider/slider3.jpg';
+
+const Slider = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={600}
+                    src={slider1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={600}
+                    src={slider2}
+                    alt="First slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={600}
+                    src={slider3}
+                    alt="First slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Slider;
