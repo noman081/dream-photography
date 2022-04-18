13abe80e1719aec861c550fd89648a4efa343b90
noman081 <abdullahalnoman642@gmail.com>
2022-04-18 22:05:00 +0600

    After adding review section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Categories from '../Categories/Categories';
+import Reviews from '../Reviews/Reviews';
 import Slider from '../Slider/Slider';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <Categories></Categories>
+            <Reviews></Reviews>
         </div>
     );
 };
