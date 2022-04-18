2534dc94952ac0190eb867ba06bcabc02816be28
noman081 <abdullahalnoman642@gmail.com>
2022-04-18 22:42:37 +0600

    After email verification and reset password

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
@@ -10,19 +10,22 @@ const Login = () => {
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
     let errorMessage;
+    const [resetError, setResetError] = useState('')
+    const [email, setEmail] = useState('');
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
-
+    const [sendPasswordResetEmail, sending, error1] = useSendPasswordResetEmail(
+        auth
+    );
 
     if (user) {
         navigate(from, { replace: true });
     }
     if (error) {
-        debugger;
         errorMessage = <p className='text-danger'>{error.message}</p>;
     }
     const handleSubmit = event => {
@@ -31,12 +34,25 @@ const Login = () => {
         const password = event.target.password.value;
         signInWithEmailAndPassword(email, password);
     }
+    const emailOnChange = event => {
+        setEmail(event.target.value);
+    }
+    const handleResetPassword = event => {
+        debugger;
+        if (email) {
+            sendPasswordResetEmail(email);
+            setResetError('');
+        }
+        else {
+            setResetError('Please enter your email');
+        }
+    }
     return (
         <div className='container w-50 mx-auto shadow-lg p-4 my-5'>
             <h2 className='text-primary text-center my-2'>Please Login</h2>
             <Form className='mt-3' onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                    <Form.Control onChange={emailOnChange} type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Control type="password" name='password' placeholder="Password" required />
@@ -46,8 +62,9 @@ const Login = () => {
                     Login
                 </Button>
             </Form>
+            <p className='text-danger'>{resetError}</p>
             <p>New to Dream Photography? <Link to="/register" className='text-primary pe-auto text-decoration-none'>Please Register</Link> </p>
-            <p className='mb-4'>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button> </p>
+            <p className='mb-4'>Forget Password? <button onClick={handleResetPassword} className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button> </p>
 
             <SocialLogin></SocialLogin>
         </div>
